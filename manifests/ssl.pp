de1e57fa2ff8adb5d43d777768d95198cd2fdbcb
Benjamin Dos Santos <benj.ds@gmail.com>
2012-11-05 16:53:15 +0100

    Add namespace to top-scope variables

--- manifests/ssl.pp
+++ manifests/ssl.pp
@@ -7,14 +7,14 @@ class vsftpd::ssl {
   exec { 'generate-certs':
     command => "/usr/bin/openssl req -new -inform PEM -x509 -nodes -days 999 -subj \
                 '/C=FR/ST=AutoSign/O=AutoSign/localityName=AutoSign/commonName=${::fqdn}/organizationalUnitName=AutoSign/emailAddress=AutoSign/' \
-                -newkey rsa:2048 -out ${vsftpd::rsa_cert_file} -keyout ${rsa_private_key_file}",
+                -newkey rsa:2048 -out ${vsftpd::rsa_cert_file} -keyout ${vsftpd::rsa_private_key_file}",
     unless  => "/usr/bin/test -f ${vsftpd::rsa_cert_file} &&
-                /usr/bin/test -f ${rsa_private_key_file}",
+                /usr/bin/test -f ${vsftpd::rsa_private_key_file}",
     require => Package['vsftpd', 'openssl'],
     notify  => Service['vsftpd'],
   }
 
-  file { ["${vsftpd::rsa_cert_file}", "${rsa_private_key_file}"]:
+  file { [$vsftpd::rsa_cert_file, $vsftpd::rsa_private_key_file]:
     ensure  => present,
     owner   => 'root',
     group   => 'root',
