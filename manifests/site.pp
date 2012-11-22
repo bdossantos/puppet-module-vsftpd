80906d42864673f0598da7cd39879d22868685bd
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2012-11-22 10:03:27 +0100

    Disable SSL and set local_root in site.pp example

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,10 @@
 node default {
-  include vsftpd
+  class { 'vsftpd':
+    force_local_data_ssl    => 'NO',
+    force_local_logins_ssl  => 'NO',
+    user_sub_token          => '$USER',
+    local_root              => '/home/$USER',
+  }
 
   vsftpd::users { 'foo':
     fullname  => 'Foo Bar'
